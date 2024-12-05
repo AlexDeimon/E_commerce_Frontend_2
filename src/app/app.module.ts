b5b89c6eb715532be75f9005791a1d15614085e5
AlexDeimon <diegoa-sandovalf@unilibre.edu.co>
2024-12-04 22:51:56 -0500

    Iniciando proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LogoComponent } from './logo/logo.component';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ShoppingCarComponent } from './shopping-car/shopping-car.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LogoComponent,
+    HomeComponent,
+    ProductDetailComponent,
+    ShoppingCarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
