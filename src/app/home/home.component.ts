6e8cac8f1f6e3436741d8a434888364a0ee48b88
AlexDeimon <diegoa-sandovalf@unilibre.edu.co>
2024-12-08 21:42:17 -0500

    Boton de ruta carrito

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  carritoId: number;
+
+  constructor(private router: Router) {
+    this.carritoId = 1;
+  }
+  goCarrito(): void {
+    console.log('Ir al carrito');
+    this.router.navigate(['/carrito', this.carritoId]);
+  }
 }
--- src/app/shopping-car/shopping-car.component.ts
+++ src/app/shopping-car/shopping-car.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-shopping-car',
   templateUrl: './shopping-car.component.html',
   styleUrls: ['./shopping-car.component.css']
 })
-export class ShoppingCarComponent {
+export class ShoppingCarComponent implements OnInit {
+  carritoId: string | null = null;
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.carritoId = this.route.snapshot.paramMap.get('carritoId');
+  }
 }
