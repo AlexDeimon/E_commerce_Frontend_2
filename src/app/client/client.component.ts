b9b7d08a2cefd69ca930e7efdd7bac5560cc156e
AlexDeimon <diegoa-sandovalf@unilibre.edu.co>
2025-02-08 21:32:10 -0500

    Implementando API: Obtener detalles de compras de cliente existente

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ICliente } from '../models/client.model';
+import { ICarrito } from '../models/shopping-car.model';
 import { MyApiService } from '../service/my-api.service';
+import { StorageService } from '../service/storage.service';
 
 @Component({
   selector: 'app-client',
@@ -18,9 +20,37 @@ export class ClientComponent implements OnInit{
     correo:   '',
     compras:  []
   };
+  shoppingCar: ICarrito = {
+    _id:                '',
+    idCliente:          '',
+    cantidad_productos: 0,
+    productos:          [],
+    precioTotal:        0
+  };
   newClient: boolean = false;
 
-  constructor(private route: ActivatedRoute, private _myApiService: MyApiService) { }
+  constructor(private route: ActivatedRoute, private _myApiService: MyApiService, private _storageService:StorageService) { }
+
+  getShoppingCar(id:string): void {
+    this._myApiService.getShoppingCar(id).subscribe({
+      next: (data: ICarrito) => {
+        this.shoppingCar = data;
+        this.shoppingCar.productos.forEach((producto) => {
+          this._storageService.getImageUrl(producto._id).subscribe({
+            next: (url: string) => {
+              producto.imagen = url;
+            },
+            error: (error: any) => {
+              console.error(error);
+            }
+          });
+        });
+      },
+      error: (error: any) => {
+        console.error(error);
+      }
+    });
+  }
 
   ngOnInit(): void {
       this.client._id = this.route.snapshot.paramMap.get('clienteId') || '';
